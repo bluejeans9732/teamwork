2993ef6a47c0bbda368e52c3af2d5f5bc36a0ce9
myeongchan kwon <dyd97wns@naver.com>
2023-04-02 18:13:00 +0900

    명찬이바보

--- src/component/Pwid.jsx
+++ src/component/Pwid.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Pwid() {
     return (
         <div className="h-full relative">
-            <div className="">회원가입입니다</div>
+            <div className="h-full w-full relative">
+            <div className="bg-teal-50 mx-auto mt-20 rounded-2xl border-2 border-slate-600 practice">
+                <div className="flex flex-col">
+                    <h1 className="Login_logo">회원가입</h1>
+                    
+                </div>
+            </div>
+            
+        </div>
         </div>
     )
 }
